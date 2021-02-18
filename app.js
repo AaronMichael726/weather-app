38dc04ebbfb5caf64125c944e0de69f50211be60
aaronmichael726 <aaronmichael726@gmail.com>
2021-02-18 17:45:42 -0600

    put together weather app in web server folder, moved the folder to the weather-app directory.  UNfortunately forget about all the commits

--- app.js
+++ app.js
@@ -1,27 +1,28 @@
 // imports
-const request = require('request')
 const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
 
-const url = 'http://api.weatherstack.com/forecast?access_key=6f624bda74801caac6a7f957b71a677d&query=37.8267,-122.4233&units=f'
-// request({ url:url, json: true }, (error, response) => {
-//     console.log(response.body.current)
-// })
+const address = process.argv[2] // note argv is the comment in the cli
 
-// request({
-//     url, 
-//     json: true
-// }, (error, response) => {
-//     if (error){
-//         console.log('Unable to connect to weather service!')
-//     } else if (response.body.error){
-//         console.log('Unable to find location')
-//     } else {
-//         console.log(`${response.body.current.weather_descriptions[0]}. It is currently ${response.body.current.temperature} degrees out. It feels like ${response.body.current.feelslike} degrees outside`)
-//     }
+if (!address) {
+    console.log('Please provide an address')
+} else { 
+    geocode(address, (error, { latitude, longitude, location } = {}) => {
+
+        if (error) {
+            return console.log(error)
+        }
     
-// })  
+        forecast(latitude, longitude, (error, forecastData) => {
+            if (error) {
+                return console.log(error)
+            }
+    
+            console.log(location)
+            console.log(forecastData)
+        })
+    
+    })
+}
 
-geocode('Boston', (error, data) => {
-    console.log('Error', error)
-    console.log('Data', data)
-})
+
